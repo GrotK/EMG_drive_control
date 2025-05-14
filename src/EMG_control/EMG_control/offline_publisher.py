6ce07fe4a7f7829a3e0e19aa8fd7abc4b6976cc8
GrotK <krza39@gmail.com>
2025-05-14 18:25:38 +0000

    data flattened

--- src/EMG_control/EMG_control/offline_publisher.py
+++ src/EMG_control/EMG_control/offline_publisher.py
@@ -11,8 +11,9 @@ class EMGPublisherNode(Node):
 
         self.publisher = self.create_publisher(Float32MultiArray, '/EMG_signal', 10)
 
-        self.data = np.load('repeats_short_X.npz')['arr_0']
+        data_path='/workspace/Dataset/sequential_X.npz'
 
+        self.data = self.flatten_overlapping_windows(np.load(data_path)['arr'])
         self.index = 0
 
         self.timer = self.create_timer(0.5, self.publish_sample)
@@ -30,6 +31,18 @@ class EMGPublisherNode(Node):
             self.get_logger().info('All samples published.')
             self.destroy_node()  
 
+    def flatten_overlapping_windows(self, data, stride=512):
+        num_windows, window_size, num_channels = data.shape
+        result = []
+
+        result.append(data[0])
+
+        for i in range(1, num_windows):
+            result.append(data[i][-stride:])
+
+        flattened_data = np.concatenate(result, axis=0) 
+        print(np.shape(flattened_data))
+        return flattened_data
 
 def main(args=None):
     rclpy.init(args=args)
